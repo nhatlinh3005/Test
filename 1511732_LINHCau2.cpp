7cdbae3908bdd4895eca40fa88e3ff5e806dfb06
nhatlinh3005 <nhatlinh3005@users.noreply.github.com>
2017-06-07 09:47:29 +0700

    Add files via upload

---
+++ 1511732_LINHCau2.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+int main() {
+
+	int n;
+	cout << "Nhap vao so tu nhien N (0 <= N <= 200):  ";
+	cin >> n;
+	while (n>200 || n<0) {
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << "nhap lai so tu nhien N: ";
+		cin >> n;
+	}
+
+	int s = 0;
+	int a = 1, i = 0;
+	while (a < 2 * n + 1) {
+		a = 2 * i + 1;
+		if (i == n) {
+			s = pow(-1, n)*pow(a, 3) + s;
+		}
+		else if (i % 2 == 0) {
+			s = pow(-1, 2)*pow(a, 3) + s;
+		}
+		else {
+			s = pow(-1, 1)*pow(a, 3) + s;
+		}
+		i += 1;
+	}
+	cout << s << endl;
+	system("pause");
+	return 0;
+}
