360ff5e560c13146d7c1448ecc38943dc334adb7
NLBKu3005 <nhatlinh5003@gmail.com>
2017-06-07 11:01:16 +0700

    add cau4

---
+++ 1511732_LINHCau4.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+#include <sstream>
+struct SINH_VIEN
+{
+	string Ten;
+	double mssv;
+	double nienkhoa;
+};
+#define MAX 50
+int main() {
+	SINH_VIEN A[MAX];
+	string str;
+	double mssv, nienkhoa;
+	getline(cin, str);
+	stringstream  ss(str);
+
+
+	for (int i = 0; i < 3; i++) {
+		getline(cin, str); 	stringstream scin1(str);
+		scin1 >> A[i].Ten;
+		getline(cin, str); stringstream scin2(str);
+		scin2 >> A[i].mssv;
+		getline(cin, str); stringstream scin3(str);
+		scin3 >> A[i].nienkhoa;
+	}
+
+	cout << A[0].Ten << " " << A[0].mssv << endl;
+	system("pause");
+	return 0;
+}
